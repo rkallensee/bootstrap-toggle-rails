a6c456b61373c8f19f410cc70ffdf25cf55dd05e
Raphael Kallensee <raphael@kallensee.name>
2016-01-11 14:21:00 +0100

    Initial commit, empty gem created by "bundle gem".

---
+++ lib/bootstrap/toggle/rails.rb
@@ -0,0 +1,9 @@
+require "bootstrap/toggle/rails/version"
+
+module Bootstrap
+  module Toggle
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bootstrap/toggle/rails/version.rb
@@ -0,0 +1,7 @@
+module Bootstrap
+  module Toggle
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
